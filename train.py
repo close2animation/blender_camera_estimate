20c041d05a419248a7cde3d58f2bb84ba1e53dc8
close2animation <88560635+close2animation@users.noreply.github.com>
2022-02-15 19:15:46 +1000

    Update train.py

--- train.py
+++ train.py
@@ -7,15 +7,20 @@ def train():
     print('test')
     # create camera
     cam1 = camera_object('camera_1')
-    cam1.bpy_object
+    matrix = transform_Matrix_to_tensor(cam1.bpy_camera.matrix_world)
+    cam1.matrix = matrix
 
+    '''
     # create transform 
     location = torch.tensor((0, 0, 0))
     euler = torch.tensor((0, 0, 0))
     convention = 'XYZ'
+    cam1.matrix = loc_rot_to_matrix(location, euler, convention)
+    '''
 
     # apply transform to torch and bpy camera
-    cam1.matrix = loc_rot_to_matrix(location, euler, convention)
-    cam1.bpy_object.matrix_world = tensor_to_Matrix(cam1.matrix)
-    print(tensor_to_Matrix(cam1.matrix))
-    print(Matrix_to_tensor(cam1.bpy_object.matrix_world))
+    cam1.bpy_camera.matrix_world = transform_tensor_to_Matrix(cam1.matrix)
+    cam1.bpy_mesh = bpy.data.objects['Icosphere_1']
+    cam1.normalise_mesh()
+    test = tensor_to_mesh_list(cam1.mesh_norm)
+    create_object(test, 'test')
