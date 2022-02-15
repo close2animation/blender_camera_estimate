b97b09482c9bccee528d1e4eefce8cd71fea1847
close2animation <88560635+close2animation@users.noreply.github.com>
2022-02-15 18:56:47 +1000

    Update transform_utils.py

--- transform_utils.py
+++ transform_utils.py
@@ -1,7 +1,8 @@
 import bpy
 import torch
-from mathutils import Matrix
+from mathutils import Matrix, Vector
 from .pytorch3d_utils import euler_angles_to_matrix
+import bmesh
 
 
 def loc_rot_to_matrix(location, rotation, convention):
@@ -34,7 +35,7 @@ def loc_rot_to_matrix(location, rotation, convention):
     return transform
 
 
-def tensor_to_Matrix(tensor):
+def transform_tensor_to_Matrix(tensor):
     '''
     converts a 4x4 tensor into a 4x4 mathutils.Matrix
 
@@ -48,7 +49,7 @@ def tensor_to_Matrix(tensor):
     return matrix
 
 
-def Matrix_to_tensor(Matrix):
+def transform_Matrix_to_tensor(Matrix):
     '''
     converts a 4x4 mathutils.Matrix into a 4x4 tensor
 
@@ -66,3 +67,99 @@ def Matrix_to_tensor(Matrix):
 
     matrix = torch.tensor(matrix_list)    
     return matrix
+
+
+def mesh_space_to_world(mesh, matrix_world):
+    '''
+    takes vertices in the mesh space and converts them to world space
+
+    Args:
+        mesh: bpy mesh type or list or verts.
+        matrix_world: meshes 4x4 world matrix. mathutlis.Matrix
+    Returns:
+        list of mesh vertices in world space
+    '''
+    # checking if mesh type or list
+    try:
+        mesh = mesh.vertices
+        def get_value(vert):
+            return vert.co
+    except AttributeError:
+        def get_value(vert):
+            return vert
+    
+    # converting 
+    mesh_world = []
+    for vert in mesh:
+        v = matrix_world @ get_value(vert)
+        mesh_world.append(v)
+    return mesh_world
+
+
+def create_object(verts, object_name):
+    '''
+    creates an object and uses the verts list to define the mesh.
+
+    Args:
+        verts: nx3 list used to create mesh
+        object_name: string that will become the objects name
+    '''
+    # create object
+    mesh = bpy.data.meshes.new("mesh")  
+    obj = bpy.data.objects.new(object_name, mesh)  
+
+    # link to scene
+    bpy.context.collection.objects.link(obj)
+    bpy.context.view_layer.objects.active = obj
+    
+    # use bmesh to create object
+    obj.select_set(True)
+    mesh = bpy.context.object.data
+    bm = bmesh.new()
+    for v in verts:
+        v = Vector(v)
+        v.resize_3d()
+        bm.verts.new(v) 
+
+    # make the bmesh the object's mesh
+    bm.to_mesh(mesh)
+    bm.free()
+
+
+def transform_mesh_tensor(mesh, transform):
+    '''
+    takes nx4 tensor and transforms it with a 4x4 transformation matrix
+
+    Args:
+        mesh: nx4 tensor that describes a mesh
+        transform: 4x4 tensor that describes a rotation and translation
+    Returns:
+        list of mesh vertices in world space
+    '''
+    mesh_new = torch.tensor([])
+    for vert in mesh:
+        vert_new = torch.matmul(transform, vert)
+        mesh_new = torch.cat((mesh_new, vert_new.unsqueeze(0)))
+    return mesh_new
+
+
+def mesh_list_to_tensor(mesh):
+    '''
+    convert a nx3 list into a nx4 tensor. extra dim to make it homogeneous.
+
+    Args:
+        mesh: nx3 list that describes a mesh
+    Returns:
+        nx4 tensor that describes a mesh in homogeneous coordinates
+    '''
+    mesh = torch.tensor(mesh)
+    dim = mesh.shape[0]
+    ones = torch.ones((dim, 1))
+    mesh = torch.hstack((mesh, ones))
+    return mesh 
+
+def tensor_to_mesh_list(tensor):
+    mesh = tensor.detach().tolist()
+    for idx in range(tensor.shape[0]):
+        del mesh[idx][3]
+    return mesh
