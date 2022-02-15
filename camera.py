9f4b2e3d552f17e673fe62d062d4a6352fd834eb
close2animation <88560635+close2animation@users.noreply.github.com>
2022-02-15 18:56:17 +1000

    Update camera.py

--- camera.py
+++ camera.py
@@ -8,15 +8,26 @@ class camera_object():
                                     [0, 1, 0, 0],
                                     [0, 0, 1, 0],
                                     [0, 0, 0, 1]])
-        self.camera_name = camera_name 
-        self.bpy_object = None
-        self.import_camera()
+        self.bpy_camera = None
+        self.bpy_mesh = None
+        self.mesh_norm = None
+        self.import_camera(camera_name)      
 
-    def import_camera(self):
+    def import_camera(self, camera_name):
         '''
         dont wanna import an object over an over
         so just assigning for now 
         '''
-        self.bpy_object = bpy.data.objects[self.camera_name]
+        self.bpy_camera = bpy.data.objects[camera_name]
+
+    def normalise_mesh(self):
+        '''
+        transforms the assgined mesh to the camera's normalised space. 
+        normalised meaning without rotation or translation.
+        '''
+        mesh = mesh_space_to_world(self.bpy_mesh.data,
+                                   self.bpy_mesh.matrix_world)
+        mesh = mesh_list_to_tensor(mesh)
+        self.mesh_norm = transform_mesh_tensor(mesh, torch.inverse(self.matrix))
 
 
