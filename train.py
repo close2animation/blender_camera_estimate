ea2d21f436af26d4af8d9cdfcf475ad8f675d1b2
close2animation <88560635+close2animation@users.noreply.github.com>
2022-02-16 16:01:01 +1000

    Update train.py

--- train.py
+++ train.py
@@ -3,24 +3,24 @@ import torch
 from .camera import camera_object
 from .transform_utils import *
 
+
+def create_transfroms(location, euler, cam):
+    location = torch.tensor(location)
+    euler = torch.tensor(euler)
+    convention = 'XYZ'
+    cam.matrix = loc_rot_to_matrix(location, euler, convention)
+
+
 def train():
     print('test')
     # create camera
-    cam1 = camera_object('camera_1')
-    matrix = transform_Matrix_to_tensor(cam1.bpy_camera.matrix_world)
-    cam1.matrix = matrix
-
-    '''
-    # create transform 
-    location = torch.tensor((0, 0, 0))
-    euler = torch.tensor((0, 0, 0))
-    convention = 'XYZ'
-    cam1.matrix = loc_rot_to_matrix(location, euler, convention)
-    '''
-
-    # apply transform to torch and bpy camera
-    cam1.bpy_camera.matrix_world = transform_tensor_to_Matrix(cam1.matrix)
+    cam1 = camera_object('camera_2')
+    cam1.matrix = Matrix_to_tensor(cam1.bpy_camera.matrix_world)
+    cam1.constant = torch.tensor([-1])
     cam1.bpy_mesh = bpy.data.objects['Icosphere_1']
+
+    cam1.update_blend_camera()
     cam1.normalise_mesh()
-    test = tensor_to_mesh_list(cam1.mesh_norm)
-    create_object(test, 'test')
+    cam1.project_mesh_to_camera_space()
+
+    create_object(cam1.image_world.tolist(), 'image')
